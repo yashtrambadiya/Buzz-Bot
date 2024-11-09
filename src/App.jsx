420000b51530fedf69a352370c2333be81934317
yashtrambadiya <yashtrambadiya008@gmail.com>
2024-11-09 17:55:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import Main from './components/Main/Main'
+
+const App = () => {
+
+
+  return (
+    <>
+      <Main/>  
+    </>
+  )
+}
+
+export default App
